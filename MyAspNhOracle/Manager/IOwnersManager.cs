5fa42a250eddeef8ddd6ead02cbd9188224072b9
ejchen <785019257@qq.com>
2022-06-19 19:26:33 +0800

    添加项目文件。

---
+++ MyAspNhOracle/Manager/IOwnersManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyAspNhOracle.Models;
+
+namespace MyAspNhOracle.Manager
+{
+    interface IOwnersManager
+    {
+        void Add(Owners owners);
+        void Update(Owners owners);
+        void Remove(Owners owners);
+        Owners GetById(int id);
+        Owners GetByOwners(string name);
+        ICollection<Owners> GetAllOwners();
+
+        //public IList<Owners> LoadByPage(int pageIndex, int pageSize, out int totalCount)
+        //{
+        //    using (var session = NhibernateHelper.OpenSession())
+        //    {
+        //        totalCount = session.QueryOver<Owners>().RowCount();	//总条数
+        //        return session.QueryOver<Owners>().Skip((pageIndex - 1) * pageSize).Take(pageSize).List();
+        //    }
+        //}
+    }
+}
---
+++ MyAspNhOracle/Manager/OwnersManager.cs
@@ -0,0 +1,70 @@
+﻿using MyAspNhOracle.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyAspNhOracle.Manager
+{
+    public class OwnersManager : IOwnersManager
+    {
+        public void Add(Owners owners)
+        {
+            using (var session = NhibernateHelper.OpenSession())
+            {
+                using (var tx = session.BeginTransaction())
+                {
+                    session.Save(owners);
+                    tx.Commit();
+                }
+            }
+        }
+
+        public ICollection<Owners> GetAllOwners()
+        {
+            using (var session = NhibernateHelper.OpenSession())
+            {
+                var owners = session.Get<Owners>(3);
+                Console.WriteLine(owners.Id + "-----" + owners.Name + "---" + owners.AddDate);
+                //Console.ReadKey();
+            }
+        }
+
+        public Owners GetById(int id)
+        {
+            using (var session = NhibernateHelper.OpenSession())
+            {
+                var owners = session.Get<Owners>(id);
+                Console.WriteLine(owners.Id + "-----" + owners.Name + "---" + owners.AddDate);
+                //Console.ReadKey();
+                return owners;
+            }
+        }
+
+        public Owners GetByOwners(string name)
+        {
+        }
+
+        public void Remove(Owners owners)
+        {
+        }
+
+        public void Update(Owners owners)
+        {
+        }
+
+        //public List<Owners> GetListByPage(int pageIndex, int pageSize)
+        //{
+        //    int totalCount;	//总条数
+        //    var productIList = productDao.LoadByPage(pageIndex, pageSize, out totalCount);
+        //    List<ProductVM> productListByPage = (from p in productIList
+        //                                         select new ProductVM
+        //                                         {
+        //                                             ID = p.ID,
+        //                                             Code = p.Code,
+        //                                             Name = p.Name,
+        //                                         }).ToList();
+        //    return productListByPage;
+        //}
+    }
+}
